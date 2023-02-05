2345aec8df81546079d453db0a507ea49bcdc632
Yasser Angel <yasserangelgm@gmail.com>
2023-02-05 13:57:55 -0600

    Test Deploy with Error Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.page';
+import Error from './pages/error.page';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/u-camp-proyecto-5/" element={<Home />}></Route>
+          <Route path="*" element={<Error />}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/error.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <div>
+      <h2>Error</h2>
+      <Link to="/u-camp-proyecto-5/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default Error;
